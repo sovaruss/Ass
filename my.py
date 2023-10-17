949d8b5a6c97a1c437e5b9c47253ed7e9bb59590
sovaruss <sovarus@gmail.com>
2023-10-18 01:30:55 +0300

    Add first files

---
+++ my.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number: "))
+b = int(input( "Also enter a number: "))
+print("Сумма=" + str(a + b))
+
+c = float(input("Enter a number: "))
+d = float(input( "Also enter a number: "))
+print("Сумма=" + str(c + d))
